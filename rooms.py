36ff8a665d683674f72f04c89c7851bf6bdd2f5b
lefriedberg <lefriedberg@mail.ru>
2019-03-18 13:02:12 -0700

    rooms.py

---
+++ rooms.py
@@ -0,0 +1,34 @@
+rooms = {
+    "at1" : 0, #at single in ste
+    "at2" : 1, #at dbl in ste
+    "at3" : 2, #at triple
+    "atef" : 3, #at efficiency
+    "ca1" : 4, #case single
+    "ca2" : 5,
+    "ca3" : 6,
+    "ca4" : 7,
+    "dws1" : 8,
+    "dws3" : 9,
+    "dwo1" : 10,
+    "dwo3" : 11,
+    "e1" : 12,
+    "e2" : 13,
+    "l22s" : 14,
+    "l22d" : 15,
+    "l41s" : 16,
+    "l41d" : 17,
+    "n1" : 18,
+    "n2" : 19,
+    "so31s" : 20, #sontag single in a suite of 3 singles, 1 double
+    "so31d" : 21,
+    "so21s" : 22,
+    "so21d" : 23,
+    "so11s" : 24,
+    "so11d" : 25,
+    "s4s" : 26, #south single in a 4-man ste
+    "s3s" : 27,
+    "s2s" : 29,
+    "s2"  : 30, #south double
+    "w1" : 31,
+    "w2" : 32
+}
